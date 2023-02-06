40dd54360ba2408545fcfc47dcb498026fd01478
pedromsrocha <sabino326@gmail.com>
2023-02-06 12:29:08 +0000

    Write surface info to any Writer

--- ch3/surface/main.go
+++ ch3/surface/main.go
@@ -9,7 +9,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math"
+	"os"
 )
 
 const (
@@ -24,20 +26,24 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	surface(os.Stdout)
+}
+
+func surface(out io.Writer) {
+	io.WriteString(out, fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
+		"width='%d' height='%d'>", width, height))
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			io.WriteString(out, fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy))
 		}
 	}
-	fmt.Println("</svg>")
+	io.WriteString(out, fmt.Sprintln("</svg>"))
 }
 
 func corner(i, j int) (float64, float64) {
